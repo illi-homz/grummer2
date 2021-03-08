2a65ad25c4ece310b1edb6c235bf48532381ef0d
illi-homz <gopkabel@mail.ru>
2021-03-08 15:27:40 +0300

    before testing

--- #src/js/questions.js
+++ #src/js/questions.js
@@ -5,10 +5,7 @@ grummer.questions = {
 
     this.$spoiler = $('.questions__spoiler')
     this.items = this.$spoiler.find('.questions__item')
-    this.items.first()
-      .addClass('active')
-      .find('.questions__item-text')
-      .slideDown(300)
+    // this.items.first().find('.questions__item-text')
   },
   initItems()
   {
@@ -30,11 +27,25 @@ grummer.questions = {
   toggleText(el)
   {
     const $el = $(el)
+    const $parent = $el.parent('.questions__item')
+    const $text = $el.siblings('.questions__item-text')
 
-    this.$spoiler.find('.questions__item-text').slideUp(300)
-    this.items.removeClass('active')
-
-    $el.siblings('.questions__item-text').slideToggle(300)
-    $el.parent('.questions__item').addClass('active')
-  }
+    $parent.hasClass('active')
+      ? this.slideUp($parent, $text)
+      : this.slideDown($parent, $text)
+  },
+  slideUp($parent, $text)
+  {
+    $parent.removeClass('active')
+    $text.slideUp(300)
+  },
+  slideDown($parent, $text)
+  {
+    this.items
+      .removeClass('active')
+      .find('.questions__item-text')
+      .slideUp(300)
+    $parent.addClass('active')
+    $text.slideDown(300)
+  },
 }
--- docs/js/script.js
+++ docs/js/script.js
@@ -7125,8 +7125,7 @@ grummer.questions = {
   init() {
     this.initItems();
     this.$spoiler = $('.questions__spoiler');
-    this.items = this.$spoiler.find('.questions__item');
-    this.items.first().addClass('active').find('.questions__item-text').slideDown(300);
+    this.items = this.$spoiler.find('.questions__item'); // this.items.first().find('.questions__item-text')
   },
 
   initItems() {
@@ -7143,10 +7142,20 @@ grummer.questions = {
 
   toggleText(el) {
     const $el = $(el);
-    this.$spoiler.find('.questions__item-text').slideUp(300);
-    this.items.removeClass('active');
-    $el.siblings('.questions__item-text').slideToggle(300);
-    $el.parent('.questions__item').addClass('active');
+    const $parent = $el.parent('.questions__item');
+    const $text = $el.siblings('.questions__item-text');
+    $parent.hasClass('active') ? this.slideUp($parent, $text) : this.slideDown($parent, $text);
+  },
+
+  slideUp($parent, $text) {
+    $parent.removeClass('active');
+    $text.slideUp(300);
+  },
+
+  slideDown($parent, $text) {
+    this.items.removeClass('active').find('.questions__item-text').slideUp(300);
+    $parent.addClass('active');
+    $text.slideDown(300);
   }
 
 };
